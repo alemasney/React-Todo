5ed03841edbeb12766e069bb42bbd94176ff1bea
Alan Lemasney <alanlemasney34@yahoo.com>
2019-05-29 02:05:11 -0600

    completed TodoList project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react'
+import TodoList from './components/TodoComponents/TodoList';
+import Header from './components/layout/Header';
+import TodoForm from './components/TodoComponents/TodoForm';
+import './App.css';
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+export class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with Wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+
+  // Toggle Complete
+
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  // Delete Todo
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  }
+
+  todoForm = title => {
+    const newTodo = {
+      id: title,
+      title,
+      completed: false,
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]})
+  }
+  
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <div className="container">
+          <Header />
+          <TodoForm todoForm={this.todoForm} />
+          <TodoList todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
+
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,24 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+
+import React, { Component } from 'react'
+import Todo from './Todo';
+import PropTypes from 'prop-types';
+
+export class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                {this.props.todos.map( todo => (
+                    <Todo key={todo.id} todoItem={todo} markComplete={this.props.markComplete} 
+                        delTodo={this.props.delTodo}
+                    />
+                ))}
+            </div>
+        )
+    }
+}
+
+TodoList.propTypes = {
+    todos: PropTypes.array.isRequired
+}
+
+export default TodoList
